446a0bc4b36f3f54743ae744b078817e01f29894
Joshua Poehls <joshua@zduck.com>
2010-08-21 23:47:55 -0500

    created solution and empty projects for DotConsole, Tests and a command line Sample project

---
+++ src/DotConsole.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotConsole.Sample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
