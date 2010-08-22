f4061c8372f49a6682b464b7072cb833693dd643
Joshua Poehls <joshua@zduck.com>
2010-08-22 00:46:46 -0500

    updated MEF to Preview 9. added some notes to the Sample Program.cs file.

--- src/DotConsole.Sample/Program.cs
+++ src/DotConsole.Sample/Program.cs
@@ -7,6 +7,12 @@ namespace DotConsole.Sample
     {
         private static void Main(string[] args)
         {
+            //  todo: optionally, register assemblies that contain commands here (add them to the aggregate catalog)
+            //        so that DotConsole knows where to look
+            //        DotConsole will look in the executing assembly by default
+
+            //  todo: optionally, set a default implicit command
+            //        this will be used if no command is specified in the args
         }
     }
 }
