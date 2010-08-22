1abb8dd6c157609213298252e09dcc1c077c9967
Joshua Poehls <joshua@zduck.com>
2010-08-22 01:03:56 -0500

    added Commander class

--- src/DotConsole.Sample/Program.cs
+++ src/DotConsole.Sample/Program.cs
@@ -7,12 +7,16 @@ namespace DotConsole.Sample
     {
         private static void Main(string[] args)
         {
+            //  todo: optionally, set a default implicit command
+            //        this will be used if no command is specified in the args
+            //Commander.DefaultCommand = XYZ;
+
+            //  this will parse the args, determine the command to run and run it
+            Commander.Run();
+
             //  todo: optionally, register assemblies that contain commands here (add them to the aggregate catalog)
             //        so that DotConsole knows where to look
             //        DotConsole will look in the executing assembly by default
-
-            //  todo: optionally, set a default implicit command
-            //        this will be used if no command is specified in the args
         }
     }
 }
---
+++ src/DotConsole/Commander.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace DotConsole
+{
+    public class Commander
+    {
+        /// <summary>
+        /// Gets or sets the default (implicit) command to run
+        /// if no command is specified in the arguments.
+        /// </summary>
+        public static ICommand DefaultCommand { get; set; }
+
+        /// <summary>
+        /// This will parse the command line arguments,
+        /// determine which command to run and run it
+        /// with the correct arguments.
+        /// </summary>
+        public static void Run()
+        {
+        }
+    }
+}
