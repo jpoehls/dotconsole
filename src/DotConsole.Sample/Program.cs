a477ea8cc19bdaefd48b9147095f79445faa6f4a
Joshua Poehls <joshua@zduck.com>
2011-04-08 07:33:37 -0500

    Minor update to Sample.

--- src/DotConsole.Sample/Program.cs
+++ src/DotConsole.Sample/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace DotConsole.Sample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var commander = Commander.Standard();
             commander.Run();
